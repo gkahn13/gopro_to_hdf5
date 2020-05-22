7d49fe11a370ced971dd04089b7defa5bb1eefda
Gregory Kahn <gkahn13@gmail.com>
2020-05-21 22:57:13 -0700

    dualfisheye calibration good

--- equirectangular_to_dualfisheye.py
+++ equirectangular_to_dualfisheye.py
@@ -37,7 +37,6 @@ def equirectangular_to_dualfisheye(im_equi, shape_dualfish, remap=None, keep_mas
 
         remap = np.stack([im_equi.shape[0] * src_v, im_equi.shape[1] * src_u], axis=-1).astype(np.float32)
 
-    # just this part is 25Hz for 1024x2048
     im_dualfish = cv2.remap(im_equi,
                             remap[..., 1],
                             remap[..., 0],
@@ -45,73 +44,11 @@ def equirectangular_to_dualfisheye(im_equi, shape_dualfish, remap=None, keep_mas
     im_dualfish[np.logical_not(keep_mask)] = [0, 0, 0]
 
     return im_dualfish, remap, keep_mask
-
-def SLOW_equirectangular_to_dualfisheye(im_equi, shape_dualfish, remap=None, keep_mask=None,
-                                   interpolation=cv2.INTER_CUBIC):
-    assert im_equi.shape[1] / im_equi.shape[0] == 2
-    assert shape_dualfish[1] / shape_dualfish[0] == 2
-
-    im_equi = np.roll(im_equi, im_equi.shape[1] // 4, axis=1)
-
-    def angular_position(up, v):
-        # correct for hemisphere
-        if up >= 0.5:
-            u = 2.0 * (up - 0.5)
-        else:
-            u = 2.0 * up
-
-        # ignore points outside of circles
-        if ((u - 0.5) * (u - 0.5) + (v - 0.5) * (v - 0.5)) > 0.25:
-            return None, None
-
-        # v: 0..1-> vp: -1..1
-        phi = np.arcsin(2.0 * (v - 0.5))
-
-        # u = math.cos(phi)*math.cos(theta)
-        # u: 0..1 -> upp: -1..1
-        u = 1.0 - u
-        theta = np.arccos(np.clip(2.0 * (u - 0.5) / np.cos(phi), -1., 1.))
-
-        if up < 0.5:
-            theta = theta - np.pi
-
-        return (theta, phi)
-
-    if remap is None or keep_mask is None:
-        remap = np.zeros(list(shape_dualfish) + [2], dtype=np.float32)
-        keep_mask = np.zeros(shape_dualfish, dtype=np.bool)
-        for x in range(shape_dualfish[1]):
-            for y in range(shape_dualfish[0]):
-                targ_u = float(x) / float(shape_dualfish[1])
-                targ_v = float(y) / float(shape_dualfish[0])
-                targ_theta, targ_phi = angular_position(targ_u, targ_v)
-
-                if targ_theta is None or targ_phi is None:
-                    continue
-                keep_mask[y, x] = True
-
-                # theta: -pi..pi -> u: 0..1
-                src_u = 0.5 + 0.5 * (targ_theta / np.pi)
-                # phi: -pi/2..pi/2 -> v: 0..1
-                src_v = 0.5 + (targ_phi / np.pi)
-
-                remap[y, x] = [im_equi.shape[0] * src_v,
-                             im_equi.shape[1] * src_u]
-
-    # just this part is 25Hz for 1024x2048
-    im_dualfish = cv2.remap(im_equi,
-                            remap[..., 1],
-                            remap[..., 0],
-                            interpolation=interpolation)
-    im_dualfish[np.logical_not(keep_mask)] = [0, 0, 0]
-
-    return im_dualfish, remap, keep_mask
-
 
 def equirectangular_to_fisheye(im_equi, size, **kwargs):
-    width = int(size)
-    height = int(size * 9. / 16.)
-    assert abs(height / float(width) - 9./16.) < 1e-4
+    # width = int(size)
+    # height = int(size * 9. / 16.)
+    # assert abs(height / float(width) - 9./16.) < 1e-4
 
     shape_dualfish = [size, 2 * size]
     im_dualfish, remap, keep_mask = equirectangular_to_dualfisheye(im_equi, shape_dualfish, **kwargs)
@@ -135,20 +72,25 @@ def equirectangular_to_fisheye(im_equi, size, **kwargs):
     # im_fish_clip = np_utils.imrectify_fisheye(im_fish_clip, K, D, balance)
 
 
-    fx, fy, cx, cy = 0.5*size, 0.5*size, 0.5*size, 0.5*size
-    # fx, fy, cx, cy = 160., 160., 160., 160.
+    assert size == 1000
+    # fx, fy, cx, cy = [499.2896289085027, 500.25930528321345, 500.39316178263533, 497.7380469112018]
+    fx, fy, cx, cy = [500., 500., 500., 500.]
     K = np.array([[fx, 0., cx],
                   [0., fy, cy],
                   [0., 0., 1.]])
-    D =  2 * np.array([[-0.038483, -0.010456, 0.003930, -0.001007]]).T
-    balance = 0.5
-    im_fish_clip = np_utils.imrectify_fisheye(im_fish, K, D, balance)
+    D = 0.9 * np.array([[-0.14978641102360382, -0.012433353097853287, 0.01134655115827489, -0.0028248970180605]]).T
+    balance = 1.0
+    im_fish_rect = np_utils.imrectify_fisheye(im_fish, K, D, balance)
+
+
+    im_fish_clip = im_fish_rect[225:-225,100:-100]
 
 
     print('plotting!')
-    f, axes = plt.subplots(1, 2)
+    f, axes = plt.subplots(1, 3)
     axes[0].imshow(im_fish)
-    axes[1].imshow(im_fish_clip)
+    axes[1].imshow(im_fish_rect)
+    axes[2].imshow(im_fish_clip)
     plt.show()
     import sys; sys.exit(0)
 
@@ -159,22 +101,23 @@ def equirectangular_to_fisheye(im_equi, size, **kwargs):
 
 
 
-im_equi = np.array(Image.open('/home/gkahn/source/vrProjector/gopro/equirectangular/0001.jpg'))
-im_equi = np_utils.imresize(im_equi, [480, 960, 3])
-im_dualfish, remap, keep_mask = equirectangular_to_fisheye(im_equi, size=960)
+if __name__ == '__main__':
+    im_equi = np.array(Image.open('/home/gkahn/source/vrProjector/gopro/equirectangular/0001.jpg'))
+    im_equi = np_utils.imresize(im_equi, [500, 1000, 3])
+    im_dualfish, remap, keep_mask = equirectangular_to_fisheye(im_equi, size=1000)
 
-# import time
-# start = time.time()
-# for _ in range(100):
-#     im_dualfish, remap, keep_mask = equirectangular_to_dualfisheye(im_equi, [1024, 2048], remap, keep_mask,
-#                                                                    cv2.INTER_CUBIC)
-# elapsed = time.time() - start
-# print(elapsed / 100.)
-# print(100. / elapsed)
+    # import time
+    # start = time.time()
+    # for _ in range(100):
+    #     im_dualfish, remap, keep_mask = equirectangular_to_dualfisheye(im_equi, [1024, 2048], remap, keep_mask,
+    #                                                                    cv2.INTER_CUBIC)
+    # elapsed = time.time() - start
+    # print(elapsed / 100.)
+    # print(100. / elapsed)
 
-print('Showing')
-import matplotlib.pyplot as plt
-f, (ax0, ax1) = plt.subplots(2, 1)
-ax0.imshow(im_equi)
-ax1.imshow(im_dualfish)
-plt.show()
+    print('Showing')
+    import matplotlib.pyplot as plt
+    f, (ax0, ax1) = plt.subplots(2, 1)
+    ax0.imshow(im_equi)
+    ax1.imshow(im_dualfish)
+    plt.show()
---
+++ gopro_to_rosbag.py
@@ -0,0 +1,105 @@
+import argparse
+import cv2
+import os
+
+import rosbag
+import rospy
+import ros_numpy
+from sensor_msgs.msg import Image, Imu, MagneticField
+
+import equirectangular_to_dualfisheye
+import file_utils
+
+
+class StampAndMessage(object):
+
+    def __init__(self, name, stamp, message):
+        self.name = name
+        self.stamp = stamp
+        self.message = message
+
+    def __lt__(self, other):
+        return self.stamp < other.stamp
+
+
+class GoproToRosbag(object):
+
+    def __init__(self, folder):
+        assert os.path.exists(folder)
+
+        mp4_fnames = file_utils.get_files_ending_with(folder, '.mp4')
+        assert len(mp4_fnames) == 1
+        self._mp4_fname = mp4_fnames[0]
+
+        self._remap = None
+        self._keep_mask = None
+
+    ##############
+    ### Camera ###
+    ##############
+
+    def _image_iterator(self):
+        full_height, full_width = 1920, 3840
+
+        cap = cv2.VideoCapture(self._mp4_fname)
+        num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        frame_num = 0
+        seq = 0
+        while cap.isOpened() and (frame_num < num_frames):
+            frame_exists, curr_frame = cap.read()
+            frame_num += 1
+            if frame_exists and frame_num % 10 == 0:
+                stamp = rospy.Time.from_sec(1e-3 * cap.get(cv2.CAP_PROP_POS_MSEC))
+
+                curr_frame, self._remap, self._keep_mask = \
+                    equirectangular_to_dualfisheye.equirectangular_to_dualfisheye(curr_frame,
+                                                                                  [1000, 2000],
+                                                                                  remap=self._remap,
+                                                                                  keep_mask=self._keep_mask)
+                curr_frame = curr_frame[:, 1000:]
+
+                curr_frame = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
+                encoding = 'mono8'
+
+                # assert tuple(curr_frame.shape[:2]) == (full_height, full_width)
+
+                # image_msg = CompressedImage()
+                # image_msg.header.stamp = stamp
+                # image_msg.format = 'jpeg'
+                # image_msg.data = np.array(cv2.imencode('.jpg', curr_frame)[1]).tostring()
+
+                image_msg = ros_numpy.msgify(Image, curr_frame, encoding=encoding)
+                image_msg.header.stamp = stamp
+                image_msg.header.seq = seq
+
+                yield image_msg
+
+                seq += 1
+
+        cap.release()
+
+    def run(self):
+        ### iterators
+        print('Creating iterators...')
+        image_iter = self._image_iterator()
+
+        image_msg = next(image_iter)
+
+        print('Writing rosbag...')
+        bag_fname = self._mp4_fname.replace('.mp4', '')
+        bag_fname += '.bag'
+        bag = rosbag.Bag(bag_fname, 'w')
+        try:
+            while True:
+                bag.write('cam/image/raw', image_msg, t=image_msg.header.stamp)
+                image_msg = next(image_iter)
+        except StopIteration:
+            pass
+        bag.close()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('folder', type=str)
+args = parser.parse_args()
+
+GoproToRosbag(args.folder).run()
